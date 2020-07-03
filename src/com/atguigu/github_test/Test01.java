52207cd085d372d6215046e7b0f65943cfeea93f
pengdong1990 <55972074+pengdong1990@users.noreply.github.com>
2020-07-03 08:02:53 +0800

    在GitHub上修改代码

--- src/com/atguigu/github_test/Test01.java
+++ src/com/atguigu/github_test/Test01.java
@@ -5,6 +5,7 @@ public class Test01 {
 	public static void main(String[] args) {
 		System.out.println("Hello,GitHub服务器！");
 		System.out.println("Hello,GitHub服务器02");
+		System.out.println("在GitHub上修改代码");
 	}
 
 }
